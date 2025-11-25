ef93ccce5f6e075f3e660a99dadc41fa43d24956
Rakshitha H G <rakshithahgr06@gmail.com>
2025-11-25 14:01:29 +0530

    verion

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Steps from "./components/Steps";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import FloatingContact from "./components/FloatingContact";
+import GMap from "./components/GMap";
+
+export default function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="section bg-[var(--navy1)]">
+        <div className="section-content">
+          <About />
+        </div>
+      </section>
+
+      <section className="section bg-[var(--navy2)]">
+        <div className="section-content">
+          <Steps />
+        </div>
+      </section>
+
+      <section className="section bg-[var(--navy1)]">
+        <div className="section-content">
+          <Testimonials />
+        </div>
+      </section>
+
+      {/* FAQ */}
+      <section className="section bg-[var(--navy4)]">
+        <div className="section-content max-w-4xl mx-auto">
+          <FAQ />
+        </div>
+      </section>
+      <Footer />
+      <FloatingContact />
+    </div>
+  );
+}
